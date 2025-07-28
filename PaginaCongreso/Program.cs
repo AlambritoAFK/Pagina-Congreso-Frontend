a2c10d6a3dc69fc44a5feed3b445a5324c3daeab
Alan Batz <alanstyb@gmail.com>
2025-07-28 08:30:10 -0600

    Cambios en el .ignore y subvistas

--- PaginaCongreso/Program.cs
+++ PaginaCongreso/Program.cs
@@ -24,9 +24,11 @@
 
             app.UseAuthorization();
 
+            app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
 
             app.Run();
         }
